0236cd2a8640d157911b2b3287437c064cbf658f
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2026-05-25 15:57:51 +0200

    Add Camera callback reg/unreg

--- spincam/camera.py
+++ spincam/camera.py
@@ -29,6 +29,7 @@ from typing_extensions import Self
 
 from spincam.nodes import NodePtr
 
+from .callbacks import NodeCallbackFunc, NodeCallbackReg
 from .nodes import CategoryPtr
 from .schemas import FuncResult
 from .system import get_sys
@@ -47,6 +48,9 @@ class Camera:
             raise ValueError(msg) from e
         self._node_ptrs: dict[str, NodePtr] = self._root_node_ptrs()
         self._create_nodemap()
+        self._node_callback_reg: NodeCallbackReg = NodeCallbackReg(
+            cam_name= str(self)
+        )
 
     def __str__(self) -> str:
         return f'Camera {self.name}'
@@ -267,6 +271,27 @@ class Camera:
         for node_ptr in self._node_ptrs.values():
             node_ptr.set_value()
         return self
+
+    def register_node_callback(
+        self,
+        node_route: str,
+        func: NodeCallbackFunc
+    ) -> FuncResult:
+        node_ptr: NodePtr | None = self.get_node_ptr(node_route)
+        if node_ptr is None:
+            msg: str = f'{self}: Unable to register "{node_route}" node callback. Node not found.'
+            spincam_logger.error(msg)
+            return FuncResult.ERROR
+        return self._node_callback_reg.register(
+            func= func,
+            node_ptr= node_ptr
+        )
+
+    def unregister_node_callback(
+        self,
+        node_route: str
+    ) -> FuncResult:
+        return self._node_callback_reg.unregister(route= node_route)
 
 
 def get_available_cam_serial_numbers() -> list[str]:
